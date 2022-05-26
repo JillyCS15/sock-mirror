669ce44eb154fb9c49165c32a8fd0d30b293c8a4
Muhammad Jilham Luthfi <mjilhamluthfi150500@gmail.com>
2022-05-26 10:22:50 +0700

    pattern: change the query to get all the resources

--- pattern/views.py
+++ pattern/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
-from .models import SHACLPattern
+from .models import ClassPattern, SHACLPattern
 
 
 def pattern_page(request):
+    # check if the user is admin
     is_admin = False
     if request.user.is_superuser:
         is_admin = True
+
+    # query all pattern class and their SHACL patterns
+    result_list = []
+    pattern_list = ClassPattern.objects.all()
+    for pattern in pattern_list:
+        shacl_pattern = SHACLPattern.objects.filter(pattern_class_id=pattern.id)
+        result_list.append([pattern, shacl_pattern])
+
     context = {
         "is_admin": is_admin,
-        "patterns": SHACLPattern.objects.all(),
+        "patterns": result_list,
         }
     return render(request, 'main/pattern-page.html', context)
